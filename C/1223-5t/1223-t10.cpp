61dbde19a9bca3aa0635511dc70e2a4d9a387820
MrBBBaiXue <wch272@outlook.com>
2020-12-23 17:27:53 +0800

    Update 1223-t10.cpp

--- C/1223-5t/1223-t10.cpp
+++ C/1223-5t/1223-t10.cpp
@@ -1,29 +1,55 @@
 ﻿#include <stdio.h>
+#define SIZE 5
 using namespace std;
 
 void Sort(int *p,int len);
 
 int main()
 {
-	int mtx[5][5] = {
+	int mtx[SIZE][SIZE] = {
         {1,2,3,4,5},
         {6,7,8,9,10},
         {11,12,13,14,15},
         {16,17,18,19,20},
         {21,22,23,24,25}
     };
-    int buffer[25], *p, *q = buffer;
-    for (int i = 0; i <= 4; i++)
+    int buffer[SIZE*SIZE], (*p)[SIZE] = mtx, *q = buffer;
+    for (int i = 0; i <= SIZE - 1; i++)
     {
-        for (int j = 0; j <= 4; j++)
+    	for (int j = 0; j <= SIZE - 1; j++)
         {
-            *q = *(*(p + i) + j);
-            *(*(p + i) + j) = -1;
+            *q = *( *(p + i) + j );
+            *( *(p + i) + j ) = -1;
             *q++;
         }
     }
     q = buffer;
-    Sort(q,25);
+    Sort(q,SIZE);
+    *(*(p + 0) + 0) = *q++;
+    *(*(p + 0) + SIZE - 1) = *q++;
+    *(*(p + SIZE - 1) + 0) = *q++;
+    *(*(p + SIZE - 1) + SIZE - 1) = *q++;
+    *(*(p + SIZE / 2) + SIZE / 2) = *(q + 20);
+    for (int i = 0; i <= SIZE - 1; i++)
+    {
+    	for (int j = 0; j <= SIZE - 1; j++)
+    	{
+    		if (*(*(p + i) + j) == -1)
+    		{
+    			*( *(p + i) + j ) = *q++;
+			}
+		}
+	}
+	//output
+	for (int i = 0; i <= SIZE - 1; i++)
+    {
+    	for (int j = 0; j <= SIZE - 1; j++)
+    	{
+    		printf("%4d",*( *(p + i) + j ));
+		}
+		printf("\n");
+	}
+	return 0;
 }
 
 void Sort(int *p,int len)
@@ -32,7 +58,13 @@ void Sort(int *p,int len)
     {
         for (int j = i; j <= len - 1; j++)
         {
-            
+            if (*(p + i) > *(p + j))
+            {
+                int t;
+                t = *(p + i);
+                *(p + i) = *(p + j);
+                *(p + j) = t;
+            }
         }
     }
     return;
