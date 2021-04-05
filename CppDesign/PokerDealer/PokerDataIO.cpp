21792ee259995ea3f90d7ae3c471e77265b3da03
MrBBBaiXue <wch272@outlook.com>
2021-04-05 16:39:17 +0800

    写了PokerDataIO，用于存储IO数据

---
+++ CppDesign/PokerDealer/PokerDataIO.cpp
@@ -0,0 +1,8 @@
+#include "PokerDataIO.h"
+#include <iostream>
+
+void OutputTestMsg()
+{
+	std::cout << "PokerDataIO.h\\OutputTestMsg triggered!" << std::endl;
+	return;
+}
--- CppDesign/PokerDealer/PokerDealer.cpp
+++ CppDesign/PokerDealer/PokerDealer.cpp
@@ -1,20 +1,35 @@
-﻿// PokerDealer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿/****************************************************************
+	P O K E R D E A L E R
+
+	File Name:     PokerDealer.cpp
+
+	Author:        Chenhao Wang (MrBBBaiXue@github.com)
+				   Boyan Wang (JingNianNian@github.com)
+				   Wenle Zhang (Skywb@github.com)
+				   Sen Ma
+
+	Version:       0.9
+
+	Date:          2021-04-05
+
+	Description:   Randomly arrange poker and display.
+
+	Classes:       
+
+****************************************************************/
 
 #include <iostream>
+#include <fstream>
+#include <time.h>
 
+// Headers in project
+#include "PokerDataIO.h"
+
+/// <summary>
+/// 程序的入口点
+/// </summary>
+/// <returns>%ERRORLEVEL%</returns>
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
