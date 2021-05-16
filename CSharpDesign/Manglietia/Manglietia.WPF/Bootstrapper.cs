30f68590313be12ce333a17f09fbbd85f77f30c6
MrBBBaiXue <wch272@outlook.com>
2021-05-16 20:14:18 +0800

    ☢ Init Manglietia.WPF and Manglietia.API

---
+++ CSharpDesign/Manglietia/Manglietia.WPF/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stylet;
+
+namespace Manglietia.WPF
+{
+    public class Bootstrapper : Bootstrapper<ViewModels.MainViewModel>
+    {
+
+
+    }
+}
---
+++ CSharpDesign/Manglietia/Manglietia.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stylet;
+
+namespace Manglietia.WPF.ViewModels
+{
+    public class MainViewModel
+    {
+        public string WindowTitle { get; set; } = "Manglietia 学生成绩管理系统";
+
+        public MainViewModel()
+        {
+
+            return;
+        }
+
+    }
+}
