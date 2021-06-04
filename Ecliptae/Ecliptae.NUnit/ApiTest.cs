1879fdd05fbc24d438a7c426016814792d0d9b07
MrBBBaiXue <wch272@outlook.com>
2021-06-05 01:53:05 +0800

    🔨 新增单元测试

---
+++ Ecliptae/Ecliptae.NUnit/ApiTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Ecliptae.NUnit
+{
+    public class ApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void VersionTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
