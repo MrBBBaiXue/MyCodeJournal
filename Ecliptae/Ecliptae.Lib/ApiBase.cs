f16b6da53c3206139a7070af4cf5b7fffe000e56
MrBBBaiXue <wch272@outlook.com>
2021-06-07 00:42:38 +0800

    update Api test

---
+++ Ecliptae/Ecliptae.Lib/ApiBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Ecliptae.Lib
+{
+    public class ApiBase
+    {
+        public static string ApiVersion() => "v1.0";
+    }
+}
--- Ecliptae/Ecliptae.Lib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ecliptae.Lib
-{
-    public class Class1
-    {
-    }
-}
