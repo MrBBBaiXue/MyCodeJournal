5dd4b74422e92d9bcea5c9934836217ebf5af958
MrBBBaiXue <wch272@outlook.com>
2021-06-22 15:43:52 +0800

    🔨 创建Stylet，删除Avalonia

--- Ecliptae/Ecliptae.Wpf/Views/LoginView.xaml.cs
+++ Ecliptae/Ecliptae.Wpf/Views/LoginView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Ecliptae.Wpf
+namespace Ecliptae.Wpf.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// LoginView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
