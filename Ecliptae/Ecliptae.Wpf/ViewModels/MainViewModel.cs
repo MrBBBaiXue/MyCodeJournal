76da493f8f7f0a28129828114ac01abf31f29d08
MrBBBaiXue <wch272@outlook.com>
2021-06-30 00:42:14 +0800

    随便改改

--- Ecliptae/Ecliptae.Wpf/ViewModels/MainViewModel.cs
+++ Ecliptae/Ecliptae.Wpf/ViewModels/MainViewModel.cs
@@ -24,7 +24,7 @@ namespace Ecliptae.Wpf.ViewModels
             var item_A = new Item
             {
                 Name = "Sandisk USB 3.0 Key (64GB)",
-                Price = 99.99F,
+                Price = 99.99,
                 Description = "IT之家 6 月 27 日消息 微软本周发布了全新的 Windows 11 操作系统，将在圣诞节期间推出正式版，并表示用户可于 2022 年免费升级到 Win11。" +
                               " 当您从 Windows 10 升级到 Windows 11 时，某些用户可能会不太习惯，例如某些长期存在的 Windows 功能被砍，其中之一是 Windows 的任务栏停靠位置。" +
                               "众所周知，在现在的多个 Windows 版本中，用户不仅可以将任务栏放到底部，还可以任意将其停靠在屏幕顶部或侧边，方便竖屏小伙伴的日常使用。",
@@ -33,7 +33,7 @@ namespace Ecliptae.Wpf.ViewModels
             var item_B = new Item
             {
                 Name = "Microsoft Windows 11 Pro",
-                Price = 888.88F,
+                Price = 888.88,
                 Storage = 552
             };
             Items.Add(item_A);
@@ -105,11 +105,7 @@ namespace Ecliptae.Wpf.ViewModels
         public bool VerifyCart()
         {
             float cost = 0.00F;
-            foreach (var orderItem in Carts)
-            {
-                cost += orderItem.Item.Price * orderItem.Count;
-            }
-            return (User.Balance >= cost);
+            return true;
         }
 
         public void SubmitPayment()
